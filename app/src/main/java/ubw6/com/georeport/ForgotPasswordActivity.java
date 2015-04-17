41289be6cc4434073f0a95b892514f6ca934db98
kjudoy <rowanatkinson23@yahoo.com>
2015-04-16 21:34:03 -0700

    clean build and removed some warnings

--- app/src/main/java/ubw6/com/georeport/ForgotPasswordActivity.java
+++ app/src/main/java/ubw6/com/georeport/ForgotPasswordActivity.java
@@ -1,9 +1,8 @@
 package ubw6.com.georeport;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,7 +11,7 @@ import android.widget.Toast;
  * Created by crystal
  * @author crystal
  */
-public class ForgotPasswordActivity extends ActionBarActivity {
+public class ForgotPasswordActivity extends Activity {
 
     Button btnSubmit;
 
@@ -37,6 +36,13 @@ public class ForgotPasswordActivity extends ActionBarActivity {
         });
     }
 
+    @Override
+    public void onBackPressed() {
+        Intent intent;
+        intent = new Intent(this, KevinLoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
 
     //@Override
     //public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,18 +51,18 @@ public class ForgotPasswordActivity extends ActionBarActivity {
     //    return true;
     //}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    //@Override
+    //public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    //    int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+     //   if (id == R.id.action_settings) {
+     //       return true;
+     //   }
 
-        return super.onOptionsItemSelected(item);
-    }
+    //    return super.onOptionsItemSelected(item);
+    //}
 }
