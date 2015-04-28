45bc48d77136e5c073458913ae728096cc3d5aad
kjudoy <rowanatkinson23@yahoo.com>
2015-04-28 14:31:44 -0700

    google maps with polyline and zoom

--- app/src/main/java/ubw6/com/georeport/MapsActivity.java
+++ app/src/main/java/ubw6/com/georeport/MapsActivity.java
@@ -14,9 +14,21 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.GoogleMap;
+//import com.google.android.gms.location.R;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.CameraUpdate;
+//import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.PolylineOptions;
+//import com.google.maps.android.PolyUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+//import com.google.android.gms.maps.model.Polyline;
+
 
 public class MapsActivity extends FragmentActivity {
 
@@ -58,7 +70,10 @@ public class MapsActivity extends FragmentActivity {
                     .getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
+               // mMap.setMyLocationEnabled(true); // shows the My location button
+               //mMap.addMarker(new MarkerOptions().position(new LatLng(mMap.getMyLocation().getLatitude(), mMap.getMyLocation().getLongitude())).title(("I'm HERE!")));
                 setUpMap();
+               // mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
             }
         }
     }
@@ -70,6 +85,54 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        List<LatLng> listPos = new ArrayList<>();
+        listPos.add(new LatLng(47.22, -122.47));
+        listPos.add(new LatLng(47.30, -122.20));
+        listPos.add(new LatLng(47.35, -121.95));
+        listPos.add(new LatLng(47.39, -122.05));
+        listPos.add(new LatLng(47.32, -122.25));
+        listPos.add(new LatLng(47.33, -122.26));
+        listPos.add(new LatLng(47.34, -122.28));
+        listPos.add(new LatLng(47.35, -122.31));
+        listPos.add(new LatLng(47.34, -122.32));
+        listPos.add(new LatLng(47.33, -122.30));
+        listPos.add(new LatLng(47.32, -122.31));
+        listPos.add(new LatLng(47.33, -122.33));
+
+
+        PolylineOptions polylineOptions = new PolylineOptions();
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for (LatLng pos: listPos) {
+            //mMap.addMarker(new MarkerOptions().position(pos).title("Marker"));
+            polylineOptions.add(pos);
+            builder.include(pos);
+        }
+        mMap.addPolyline(polylineOptions);
+        LatLngBounds bounds = builder.build();
+
+
+
+
+        //CameraPosition cameraPosition = new CameraPosition.Builder().target(
+        //        pos).zoom(12).build();
+
+
+        // draw polyline and draw marker
+        // draw again per submit
+
+
+
+        // Adjust the camera depending on the position
+
+
+
+        int padding = 0; // offset from edges of the map in pixels
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 25, 25, padding);
+        // check if its far out zoom on the first point
+        mMap.moveCamera(cu);
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        //mMap.getCameraPosition().zoom();
+
     }
+
 }
